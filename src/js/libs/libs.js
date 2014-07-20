64d1081f053abd110f1f2ad9cd614d0898024c12
Ilter Canberk <iltercanberk@gmail.com>
2014-07-20 17:02:19 -0500

    working

---
+++ src/js/libs/libs.js
@@ -0,0 +1,4 @@
+window.$, window.jQuery = require('./node_modules/jquery/dist/jquery.js');
+require('./node_modules/jquery-ui-browserify/dist/jquery-ui.js');
+window._ = require('./node_modules/underscore/underscore.js');
+window.Backbone = require('./node_modules/backbone/backbone.js');
---
+++ src/js/refactor-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    quote           :  '\''         // '\'' or '"'
+  , style           :  'var'        // 'var', 'comma', 'comma-first'
+  , indent          :  2            // the tab size used in your project
+  , directoryFilter :  null         // not supported yet
+  , fileFilter      :  '.js'        // the extension of the file to upgrade
+  , dryrun          :  true         // true|false if true no changes will be written to upgraded files
+};
